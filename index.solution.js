0d91de0abfca7b16c4795901f53ee19866c342e4
BugHunter-X <askzenai@gmail.com>
2025-02-17 01:54:04 +0100

    Add index.solution.js file

---
+++ index.solution.js
@@ -0,0 +1,15 @@
+```javascript
+// pages/index.solution.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+      <Link href='/about'>
+        <a>Go to About</a>
+      </Link>
+    </div>
+  );
+}
+```
